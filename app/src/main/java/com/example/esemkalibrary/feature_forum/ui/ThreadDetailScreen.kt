a673df644e9d694c249e547938bd033ec654b7c5
dffrndik <daffa.randika.3@gmail.com>
2023-05-15 19:24:44 +0700

    added reply section

--- app/src/main/java/com/example/esemkalibrary/feature_forum/ui/ThreadDetailScreen.kt
+++ app/src/main/java/com/example/esemkalibrary/feature_forum/ui/ThreadDetailScreen.kt
@@ -6,18 +6,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
-import com.example.esemkalibrary.core.components.theme.SandBrown
+import com.example.esemkalibrary.core.components.LibraryButton
+import com.example.esemkalibrary.core.components.LibraryTextField
 
 @Composable
 fun ThreadDetailScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-        .background(color = SandBrown)
-        .fillMaxWidth()
-    ) {
+    Column {
         LazyColumn(modifier
             .weight(weight = 1f, fill = false)
             .padding(8.dp),
@@ -28,6 +27,15 @@ fun ThreadDetailScreen(modifier: Modifier = Modifier) {
             items(7) {
                 ReplyCard()
             }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = White)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            LibraryTextField(value = "", onValueChange = {}, hint = { Text("Reply....") }, modifier = Modifier.fillMaxWidth(), showLabel = false)
+            LibraryButton(onClick = { /*TODO*/ }, text = "Add Reply", modifier = Modifier.fillMaxWidth())
         }
     }
 }
