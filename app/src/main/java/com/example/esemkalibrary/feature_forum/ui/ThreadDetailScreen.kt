d8aa63fff198763dcb4cea5b0793524f73e9804a
dffrndik <daffa.randika.3@gmail.com>
2023-05-16 07:06:17 +0700

    fixed mystery padding and keyboard recomposition issue

--- app/src/main/java/com/example/esemkalibrary/feature_forum/ui/ThreadDetailScreen.kt
+++ app/src/main/java/com/example/esemkalibrary/feature_forum/ui/ThreadDetailScreen.kt
@@ -1,6 +1,8 @@
 package com.example.esemkalibrary.feature_forum.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,17 +11,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
 import com.example.esemkalibrary.core.components.LibraryButton
 import com.example.esemkalibrary.core.components.LibraryTextField
+import com.example.esemkalibrary.core.components.theme.Grey
 
 @Composable
 fun ThreadDetailScreen(modifier: Modifier = Modifier) {
     Column {
         LazyColumn(modifier
             .weight(weight = 1f, fill = false)
-            .padding(8.dp),
+            .padding(top = 8.dp)
+            .padding(horizontal = 8.dp)
+            ,
             verticalArrangement = Arrangement.spacedBy(4.dp)) {
             item {
                 PostCard()
@@ -31,6 +37,7 @@ fun ThreadDetailScreen(modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
+                .border(BorderStroke(width = 1.dp, color = Grey.copy(0.5f)))
                 .background(color = White)
                 .padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(4.dp)) {
